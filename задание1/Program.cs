14d7c49c222ae70250e43f8c8027fa6b0da5d4e8
Liza <elizaveta.b.elizaveta@yandex.by>
2022-09-10 14:28:06 +0300

    задание1

---
+++ задание1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите числа: ");
+string numberString = Console.ReadLine();
+
+int count = 1;
+int minus = 0;
+for (int i = 0; i < numberString.Length; i++)
+{
+    if(numberString[i] == ',')
+    count++;
+    if(numberString[i] == '-')
+    minus++;
+}
+count = count-minus;
+Console.Write("Положительных чисел: " + count);
